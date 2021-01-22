07f162026b4c8a6bc24a2a5f92bb09434ae96d66
1000119577 <soumyadipg04@gmail.com>
2021-01-22 15:22:58 +0530

    add entry

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 import markdown
 
 from . import util
@@ -21,3 +23,19 @@ def title(request, title):
         "text":text,
         "title":title
     })
+
+def page(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        if title in util.list_entries():
+            return HttpResponse("already exist")
+        elif title =="" or request.POST['content'] =="":
+            return HttpResponse("please feel all")
+        
+        with open(f"entries/{title}.md", 'w') as f:
+            f.write(request.POST["content"])
+        url = reverse('title', kwargs={'title': title})
+        return HttpResponseRedirect(url)
+        #return HttpResponseRedirect(reverse("title"))
+
+    return render(request, "encyclopedia/new.html")

a50ac48fdebfd09af040ad3b404652235287b9d6
1000119577 <soumyadipg04@gmail.com>
2021-01-22 11:46:57 +0530

    markdown inserted and hyperlinks added in index

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import markdown
 
 from . import util
 
@@ -8,3 +10,14 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def title(request, title):
+    text = util.get_entry(title)
+    if text is None:
+        return HttpResponse("no such file")
+    md = markdown.Markdown()
+    text = md.convert(text)
+ #   return HttpResponse(f"hello {title}")
+    return render(request, "encyclopedia/info.html", {
+        "text":text,
+        "title":title
+    })
